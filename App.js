1c2b6dcaba4efbf10aa22fdf94c3e79f3045005c
Veronica Minciuna <veronicaminci@gmail.com>
2020-07-30 22:53:30 -0400

    front-end

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
 import React from "react";
-// import AccountScreen from "./app/screens/AccountScreen";
-// import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-// import ListingEditScreen from "./app/screens/ListingEditScreen";
-// import ListingsScreen from "./app/screens/ListingsScreen";
-// import LoginScreen from "./app/screens/LoginScreen";
-// import RegisterScreen from "./app/screens/RegisterScreen";
-// import MessagesScreen from "./app/screens/MessagesScreen";
+
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 //mapped to UI View of iOS
