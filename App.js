315094eb6d98dfe83c171d9e6e454ef920e60210
Veronica Minciuna <veronicaminci@gmail.com>
2020-07-30 22:50:33 -0400

    app front end

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+// import AccountScreen from "./app/screens/AccountScreen";
+// import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+// import ListingEditScreen from "./app/screens/ListingEditScreen";
+// import ListingsScreen from "./app/screens/ListingsScreen";
+// import LoginScreen from "./app/screens/LoginScreen";
+// import RegisterScreen from "./app/screens/RegisterScreen";
+// import MessagesScreen from "./app/screens/MessagesScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
-//mapped to UIView of iOS
+//mapped to UI View of iOS
 export default function App() {
-  return <ListingsScreen />;
+  return <WelcomeScreen />;
 }
