e1199cff2146e445fc417f8ff88257e66b23112f
Veronica Minciuna <veronicaminci@gmail.com>
2020-07-31 23:16:05 -0400

    app changes

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import ListingsScreen from "./app/screens/ListingsScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 //mapped to UI View of iOS
+
 export default function App() {
   return <WelcomeScreen />;
 }
