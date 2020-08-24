3ea877b826e057233c44ea478024fdefc8ede3ab
Veronica Minciuna <veronicaminci@gmail.com>
2020-08-23 23:32:53 -0400

    listing

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,50 +1,50 @@
-import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+// import React from "react";
+// import { View, Image, StyleSheet } from "react-native";
 
-import colors from "../config/colors";
-import ListItem from "../components/lists/ListItem";
-import Text from "../components/Text";
+// import colors from "../config/colors";
+// import ListItem from "../components/lists/ListItem";
+// import Text from "../components/Text";
 
-function ListingDetailsScreen(props) {
-  return (
-    <View>
-      <Image style={styles.image} source={require("../assets/item1.jpg")} />
-      <View style={styles.detailsContainer}>
-        <Text style={styles.title}>Used table for sale</Text>
-        <Text style={styles.price}>$100</Text>
-        <View style={styles.userContainer}>
-          <ListItem
-            image={require("../assets/veronicaminci.jpg")}
-            title="Veronica Minci"
-            subTitle="2 Listings"
-          />
-        </View>
-      </View>
-    </View>
-  );
-}
+// function ListingDetailsScreen(props) {
+//   return (
+//     <View>
+//       <Image style={styles.image} source={require("../assets/item1.jpg")} />
+//       <View style={styles.detailsContainer}>
+//         <Text style={styles.title}>Used table for sale</Text>
+//         <Text style={styles.price}>$100</Text>
+//         <View style={styles.userContainer}>
+//           <ListItem
+//             image={require("../assets/veronicaminci.jpg")}
+//             title="Veronica Minci"
+//             subTitle="2 Listings"
+//           />
+//         </View>
+//       </View>
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  detailsContainer: {
-    padding: 20,
-  },
-  image: {
-    width: "100%",
-    height: 300,
-  },
-  price: {
-    color: colors.secondary,
-    fontWeight: "bold",
-    fontSize: 20,
-    marginVertical: 10,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "500",
-  },
-  userContainer: {
-    marginVertical: 40,
-  },
-});
+// const styles = StyleSheet.create({
+//   detailsContainer: {
+//     padding: 20,
+//   },
+//   image: {
+//     width: "100%",
+//     height: 300,
+//   },
+//   price: {
+//     color: colors.secondary,
+//     fontWeight: "bold",
+//     fontSize: 20,
+//     marginVertical: 10,
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: "500",
+//   },
+//   userContainer: {
+//     marginVertical: 40,
+//   },
+// });
 
-export default ListingDetailsScreen;
+// export default ListingDetailsScreen;
