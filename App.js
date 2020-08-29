02feafe674fa9d6ada21d3fc5cb8cd70d16f3be4
Veronica Minciuna <veronicaminci@gmail.com>
2020-08-29 10:55:27 -0400

    login screen

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import RegisterScreen from "./app/screens/RegisterScreen";
+import LoginScreen from "./app/screens/LoginScreen";
 
 //mapped to UI View of iOS
 
 export default function App() {
-  return <RegisterScreen />;
+  return <LoginScreen />;
 }
