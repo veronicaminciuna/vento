6cca597fae34860d03654343a78984b5e064c6e5
Veronica Minciuna <veronicaminci@gmail.com>
2020-08-30 09:38:52 -0400

    messages

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 //mapped to UI View of iOS
 
 export default function App() {
-  return <LoginScreen />;
+  return <MessagesScreen />;
 }
