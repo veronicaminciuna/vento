71ddb06ab82158243e7d3a0eddbb3d201d5b7801
Veronica Minciuna <veronicaminci@gmail.com>
2020-09-03 10:47:11 -0400

    image screen

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from "react";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 //mapped to UI View of iOS
 
 export default function App() {
-  return <RegisterScreen />;
+  return <ViewImageScreen />;
 }
