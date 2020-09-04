bec91c52e1e9fdf15aee3553ed9dd5276048af1f
Veronica Minciuna <veronicaminci@gmail.com>
2020-09-04 15:42:10 -0400

    listings

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 //mapped to UI View of iOS
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <ListingEditScreen />;
 }
