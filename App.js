eb24ae5941bd946427a6dc00237e162ee40e390e
Veronica Minciuna <veronicaminci@gmail.com>
2020-09-08 12:08:41 -0400

    view image2

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 //mapped to UI View of iOS
 
 export default function App() {
-  return <ListingEditScreen />;
+  return <WelcomeScreen />;
 }
