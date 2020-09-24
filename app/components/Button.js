89fe699417d2eacd9fc08e6a3bf7677fc2f30364
Veronica Minciuna <veronicaminci@gmail.com>
2020-09-24 11:46:07 -0400

    button

--- app/components/Button.js
+++ app/components/Button.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 import colors from "../config/colors";
 
-function AppButton({ title, onPress, color = "primary" }) {
+function AppButton({ title, onPress, color = "secondary" }) {
   return (
     <TouchableOpacity
       style={[styles.button, { backgroundColor: colors[color] }]}
@@ -18,11 +18,11 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: colors.primary,
     borderRadius: 25,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 15,
+    justifyContent: "left",
+    alignItems: "left",
+    padding: 20,
     width: "100%",
-    marginVertical: 10,
+    marginVertical: 20,
   },
   text: {
     color: colors.white,
